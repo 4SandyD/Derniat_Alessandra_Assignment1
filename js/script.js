5be05d696c3f8e7287a01ee4614a92b017d8d86a
4SandyD <24757125+4SandyD@users.noreply.github.com>
2019-02-07 20:14:04 -0800

    FEWD 644 Assignment 1
    25 picky, detail-oriented questions.

---
+++ js/script.js
@@ -0,0 +1,222 @@
+/*
+//STEP 1
+var someMonth;
+function theMonth());
+var currentMonth;
+var summerMonth;
+var myLibraryFunction;
+*/
+
+/*
+//STEP 2
+57
+"Sandy"
+true
+null
+*/
+
+
+/*
+//STEP 3
+var myName = "Sandy";
+var numKids = 2;
+*/
+
+/*
+//STEP 4
+var fName;
+var lName;
+var strAddress;
+var strCity;
+var strState;
+var numZip;
+var numAge;
+var referSrc;
+var mayWeContact;
+*/
+
+/*
+//STEP 5
+var fName;
+var lName;
+var strAddress;
+fName = "Sandy";
+lName = "Derniat";
+strAddress = "8118 House Street";
+
+var strCity = "Detroit";
+var strState = "MI";
+var numZip = 48234;
+
+var numAge = 21, var referSrc = "newspaper", var mayWeContact = true;
+*/
+
+
+/*
+//STEP 6
+var x = 2019;
+window.console.log("I predict the year " + x + " will be your best one yet!");
+*/
+
+//STEP 7
+
+
+/*
+//STEP 8
+//The string is not valid because of the apostrophe in I'm. The following corrects:
+
+var someString = "'Who once said, 'Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.'";
+window.console.log(someString);
+*/
+
+/*
+//STEP 9
+var x = 1000;
+x = null;
+window.console.log(x);
+var y;
+window.console.log(y);
+*/
+
+
+/*
+//STEP 10
+window.console.log(typeof "Sandy");
+window.console.log(typeof 666);
+window.console.log(typeof true);
+window.console.log(typeof [1,10,100,1000]); 
+window.console.log(typeof job);
+*/
+
+
+/*
+//STEP 11
+var name = "Zak Ruvalcaba";
+alert("Hello " + name + ", welcome to the JavaScript class!");
+*/
+
+/*
+//STEP 12
+var name = "Sandy Derniat";
+alert("Hello " + name + ", welcome to the JavaScript class!");
+*/
+
+/*
+//STEP 13
+var name = "Sandy Derniat";
+var course = "JavaScript";
+alert("Hello " + name + "," + " welcome to the " + course + " class!");
+*/
+
+/*
+//STEP 14
+var name = "Zak Ruvalcaba";
+var course = "JavaScript";
+alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+*/
+
+/*
+//STEP 15
+var name = window.prompt("What is your name?");
+*/
+
+/*
+//STEP 16
+var course = prompt("What course are you taking?");
+*/
+
+/*
+//STEP 17
+var x = 10;
+var y = 20;
+window.console.log(x + y);
+*/
+
+/*
+//STEP 18
+var x = 20;
+window.console.log(x += 20);
+*/
+
+/*
+//STEP 19
+var x = 20;
+window.console.log(x *= 5);
+*/
+
+/*
+//STEP 20
+var x = 20 % 3;
+window.console.log(x /= 1);
+*/
+
+/*
+//STEP 21
+var x = 10;
+var y = 100;
+window.console.log(x > 1 || y > 1000);
+*/
+
+
+/*
+//STEP 22
+var x = 10;
+var y = 100;
+window.console.log(x == 1 && y == 1000);
+*/
+
+/*
+//STEP 23
+var x = new Object("widget");
+window.console.log(typeof x);
+*/
+
+/*
+//STEP 24
+var x = new Object("widget");
+var result = x instanceof Object;
+window.console.log(result);
+*/
+
+/*
+//STEP 25
+var x = new Object("widget");
+var result = x instanceof Date;
+window.console.log(result);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
